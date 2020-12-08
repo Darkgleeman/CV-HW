c28b1cd41111249f9a02ff5d3f615d246d80941f
tianye <tianye@shanghaitech.edu.cn>
2020-12-08 11:30:22 +0800

    auto uplaod

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import glob
+import torch
+import cv2
 
-def pre_processor():
+def get_paths(directory):
     train_imgs = glob.glob("dataset/train/img/*.jpg")
     train_density = glob.glob("dataset/train/density_maps/*.jpg")
+    return (train_imgs,train_density)
 
+def pre_processor(img_path):
+    raw_img = cv2.imread(img_path)
+
+if __name__ == "__main__":
+    pass
