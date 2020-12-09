6d0b802590bac65e4ada9258e37fff6068744554
tianye <tianye@shanghaitech.edu.cn>
2020-12-09 14:18:08 +0800

    auto uplaod

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ import h5py
 import os
 import skimage.io as io
 
-
-
-def get_paths(directory):
-    train_imgs = glob.glob("dataset/train/img/*.jpg")
-    train_density = glob.glob("dataset/train/density_maps/*.jpg")
-    return (train_imgs,train_density)
+import model
+import train
+import CNN
 
 
 def get_density_maps():
@@ -35,19 +32,22 @@ def get_imgs():
     return imgs
 
 
-def pre_processor(img_path,density_path):
+def pre_processor():
     density_maps = get_density_maps()
     imgs = get_imgs()
     splited_img = [[],[],[]]
     splited_density = [[],[],[]]
+    h = 512
+    w = 384
     for raw_density, raw_img in zip(density_maps, imgs):
-        h = 512
-        w = 384
         for i in range(3):
             for j in range(3):
                 splited_img[i].append(raw_img[h//2*j:h//2*(j+2), w//2*i:w//2*(i+2)]) #split into 9 parts
                 splited_density[i].append(raw_density[h//2*j:h//2*(j+2),w//2*i:w//2*(i+2)])
     
+    
 
 if __name__ == "__main__":
-    pre_processor(None, None)
+    epoch = 100
+    for i in range(epoch):
+        pre_processor()
