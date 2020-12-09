b943b907c937229c119b2ba15f35ac9fa3dce6c2
tianye <tianye@shanghaitech.edu.cn>
2020-12-09 15:49:02 +0800

    auto uplaod

--- preprocess.py
+++ preprocess.py
@@ -46,37 +46,30 @@ def Geometry_Gaussain(shape,label):
 
 
 
+if __name__ == "__main__":
+	path="dataset/train/ground_truth/"
+	path_image="dataset/train/images/"
 
-path="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_A_final\\train_data\\ground_truth\\"
-path_image="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_A_final\\train_data\\images\\"
-
-#path="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_B_final\\train_data\\ground_truth\\"
-#path_image="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_B_final\\train_data\\images\\"
-savepath="data_preprocessed\\partB\\train\\"
-for i in range(54,55):
-	ground_truth_path=path+"GT_IMG_"+str(i)+".mat"
-	ground_truth=scio.loadmat(ground_truth_path)
-	ground_truth=ground_truth['image_info'][0,0][0,0][0]
-	shape=Image.open(path_image+"IMG_"+str(i)+".jpg").size
-	densitymap=Geometry_Gaussain(shape,ground_truth)
-	np.save(savepath+"numpyarray\\"+str(i)+".npy",densitymap)
-	print(i)
-	print(ground_truth.shape[0])
-	print(np.sum(densitymap))
-	densitymap*=250/np.max(densitymap)
-	img=densitymap.T.astype(np.uint8)
-	'''
-	img.save(savepath+"image\\"+str(i)+".jpg")
-	'''
-	io.imsave(savepath+"image\\"+str(11)+".jpg",img)
-	plt.figure(0)
-	plt.imshow(img, cmap = 'jet')
-	plt.show()
-
-
-
-
-
-
-
+	#path="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_B_final\\train_data\\ground_truth\\"
+	#path_image="dataset\\shanghaitech\\ShanghaiTech_Crowd_Counting_Dataset\\ShanghaiTech_Crowd_Counting_Dataset\\part_B_final\\train_data\\images\\"
+	savepath="data_preprocessed\\partB\\train\\"
+	for i in range(54,55):
+		ground_truth_path=path+"GT_IMG_"+str(i)+".mat"
+		ground_truth=scio.loadmat(ground_truth_path)
+		ground_truth=ground_truth['image_info'][0,0][0,0][0]
+		shape=Image.open(path_image+"IMG_"+str(i)+".jpg").size
+		densitymap=Geometry_Gaussain(shape,ground_truth)
+		np.save(savepath+"numpyarray\\"+str(i)+".npy",densitymap)
+		print(i)
+		print(ground_truth.shape[0])
+		print(np.sum(densitymap))
+		densitymap*=250/np.max(densitymap)
+		img=densitymap.T.astype(np.uint8)
+		'''
+		img.save(savepath+"image\\"+str(i)+".jpg")
+		'''
+		io.imsave(savepath+"image\\"+str(11)+".jpg",img)
+		plt.figure(0)
+		plt.imshow(img, cmap = 'jet')
+		plt.show()
 
