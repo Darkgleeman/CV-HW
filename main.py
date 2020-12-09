e39896a1e3144a94d74b9ee672f1c1db3a1871f0
tianye <tianye@shanghaitech.edu.cn>
2020-12-09 16:19:39 +0800

    auto uplaod

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import glob
 import torch
 import cv2
-import numpy as np
-
 import h5py
-import os
+import numpy as np
 import skimage.io as io
+
+################### local module ######################
 
 import model
 import train
 import CNN
 
+######################################################
 
 def get_density_maps():
     h5_names = glob.glob('dataset/train/ground-truth-h5/*.h5')
-    # sort in increasing order
-    h5_names = sorted(h5_names, key=lambda x : int(x[x.index('_')+1 : x.index('.')]))
-    # desity_maps with shape(1024, 768), np.array
-    maps = []
+    h5_names = sorted(h5_names, key = lambda x : int(x[x.index('_')+1 : x.index('.')]))# sort in increasing order
+    density_maps = []# desity_maps with shape(1024, 768), np.array
     for h5 in h5_names:
         h5 = h5py.File(h5, 'r')['density']
-        map = np.array(h5).T # transpose it to get the consistent size
-        maps.append(map)
-    return maps
+        density_map = np.array(h5).T # transpose it to get the consistent size
+        density_maps.append(density_map)
+    return density_maps
 
 
 def get_imgs():
@@ -32,25 +31,25 @@ def get_imgs():
     return imgs
 
 
-def pre_processor():
+def pre_processor():    #it is a generator
     density_maps = get_density_maps()
-    imgs = get_imgs()
-    splited_img = [[],[],[]]
+    imgs         = get_imgs()
+    splited_img  = [[],[],[]]
     splited_density = [[],[],[]]
-    h = 512
+    h = 512  # half the size
     w = 384
+    splited_img     = np.ndarray((9,3,h,w))
+    splited_density = np.ndarray((9,3,h,w))
     for raw_density, raw_img in zip(density_maps, imgs):
         for i in range(3):
             for j in range(3):
-                splited_img[i].append(raw_img[h//2*j:h//2*(j+2), w//2*i:w//2*(i+2)]) #split into 9 parts
-                splited_density[i].append(raw_density[h//2*j:h//2*(j+2),w//2*i:w//2*(i+2)])
+                splited_img[i*3+j]     = raw_img[:,h//2*j:h//2*(j+2), w//2*i:w//2*(i+2)]   #split into 9 parts
+                splited_density[i*3+j] = raw_density[:,h//2*j:h//2*(j+2),w//2*i:w//2*(i+2)]#dimension may be wrong
         yield splited_img , splited_density
-    
-    
 
 if __name__ == "__main__":
     epoch = 100
     for i in range(epoch):
         batch_iterater = pre_processor()
-        for patch_to_train , ground_truth in batch_iterater:
+        for patch_to_train , ground_truth in batch_iterater:# 2 numpy-array 
             pass
